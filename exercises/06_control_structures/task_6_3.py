91940e8eab51d7c31abb630b6df36ba914fdcee3
PolundraNovo <86477338+PolundraNovo@users.noreply.github.com>
2023-03-29 15:05:19 +0300

    Add files via upload

--- exercises/06_control_structures/task_6_3.py
+++ exercises/06_control_structures/task_6_3.py
@@ -1,28 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 Задание 6.3
-
 В скрипте сделан генератор конфигурации для access-портов.
 Сделать аналогичный генератор конфигурации для портов trunk.
-
 В транках ситуация усложняется тем, что VLANов может быть много, и надо понимать,
 что с ними делать (добавлять, удалять, перезаписывать).
-
 Поэтому в соответствии каждому порту стоит список и первый (нулевой) элемент списка
 указывает как воспринимать номера VLAN, которые идут дальше.
-
 Пример значения и соответствующей команды:
 * ['add', '10', '20'] - команда switchport trunk allowed vlan add 10,20
 * ['del', '17'] - команда switchport trunk allowed vlan remove 17
 * ['only', '11', '30'] - команда switchport trunk allowed vlan 11,30
-
 Задача для портов 0/1, 0/2, 0/4, 0/5, 0/7:
 - сгенерировать конфигурацию на основе шаблона trunk_template
 - с учетом ключевых слов add, del, only
-
 Код не должен привязываться к конкретным номерам портов. То есть,
 если в словаре trunk будут другие номера интерфейсов, код должен работать.
-
 Для данных в словаре trunk_template вывод на
 стандартный поток вывода должен быть таким:
 interface FastEthernet0/1
@@ -45,8 +38,6 @@ interface FastEthernet0/7
  switchport trunk encapsulation dot1q
  switchport mode trunk
  switchport trunk allowed vlan 30
-
-
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 На стандартный поток вывода надо выводить только команды trunk настройки,
 а access закомментировать.
@@ -73,6 +64,23 @@ trunk = {
     "0/5": ["add", "10", "21"],
     "0/7": ["only", "30"],
 }
+for intf, vlans in trunk.items():
+    print("interface FastEthernet " + intf)
+    for command in trunk_template:
+        if command.endswith("allowed vlan"):
+            out = ''
+            if vlans[0] == "add":
+                out = 'add '
+#            elif vlans[0] == "only":
+#                pass
+            elif vlans[0] == "del":
+                out = 'remove '
+            for i in range(1, len(vlans)):
+                out = out +  vlans[i] +', '
+            out = out[:-2]
+            print(f" {command} {out}")
+        else:
+            print(f" {command}")
 
 # for intf, vlan in access.items():
 #     print("interface FastEthernet" + intf)
@@ -81,4 +89,3 @@ trunk = {
 #             print(f" {command} {vlan}")
 #         else:
 #             print(f" {command}")
-
