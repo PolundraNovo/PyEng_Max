4127ef15e40ef1d2f60b0837776cbf1cdf4c68c0
PolundraNovo <86477338+PolundraNovo@users.noreply.github.com>
2023-06-06 17:06:51 +0300

    Add files via upload

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Задание 12.1
 
@@ -17,3 +16,29 @@ IP-адрес считается доступным, если выполнени
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+import subprocess
+import sys
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+#print(u'Пример вывода кириллического текста')
+
+def task_12_1(ip_addr):
+    access_ip = []
+    unaccess_ip = []
+    for item in ip_addr:
+        command = 'ping '+item
+        result = subprocess.run(command)
+        if result.returncode == 0:
+            access_ip.append(item)
+        else:
+            unaccess_ip.append(item)
+
+    return (access_ip, unaccess_ip)
+
+if __name__ == '__main__':
+
+    ip_addr = ['8.8.8.8','22.22.22.22']
+    access_ip, unaccess_ip = task_12_1(ip_addr)
+    print(access_ip)
+    print(unaccess_ip)
+
